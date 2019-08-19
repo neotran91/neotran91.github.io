5e3cd683a67f967e39311b736527fc40820a4d9a
Tran <neotran91@gmail.com>
2019-08-19 20:39:57 +0700

    190819-2039 commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+class Greeting extends React.Component {
+    render() {
+        return (<h1>Nguyễn Vương Trần </h1>);
+    }
+}
+ReactDOM.render(
+    <Greeting />,
+    document.getElementById('root')
+);
